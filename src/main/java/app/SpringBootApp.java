21ce450696bbaa59767e8f455f0f9ccba7ea962d
Firdavs Ubaydov <firdavs.94.94@mail.ru>
2017-04-15 22:24:12 +0500

    finally works war tomcat deploy!

--- src/main/java/app/SpringBootApp.java
+++ src/main/java/app/SpringBootApp.java
@@ -2,10 +2,18 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "app")
-public class SpringBootApp {
-    public static void main(String[] args) {
+public class SpringBootApp extends SpringBootServletInitializer {
+    public static void main(String[] args) /*throws Exception*/ { 
         SpringApplication.run(SpringBootApp.class, args);
     }
+
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+       return application.sources(SpringBootApp.class);
+   }
+   
 }
